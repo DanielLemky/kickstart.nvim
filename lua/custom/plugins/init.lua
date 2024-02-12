efa6f4114b3af4b951189f47c2f9efd9c4367b8d
Daniel Lemky <daniel@quantumfirelabs.com>
2024-02-12 10:26:15 -0600

    Lazygit, whitespace plugins | keymappings | lsp servers

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,37 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+
+	{
+		"kdheepak/lazygit.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+		config = function()
+			require("lazy").setup({})
+			vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<CR>")
+		end
+	},
+
+	{
+		'johnfrankmorgan/whitespace.nvim',
+		config = function ()
+			require('whitespace-nvim').setup({
+			-- configuration options and their defaults
+
+			-- `highlight` configures which highlight is used to display
+			-- trailing whitespace
+			highlight = 'DiffDelete',
+
+			-- `ignored_filetypes` configures which filetypes to ignore when
+			-- displaying trailing whitespace
+			ignored_filetypes = { 'TelescopePrompt', 'Trouble', 'help' },
+
+			-- `ignore_terminal` configures whether to ignore terminal buffers
+			ignore_terminal = true,
+			})
+
+			-- remove trailing whitespace with a keybinding
+			vim.keymap.set('n', '<leader>tw', require('whitespace-nvim').trim)
+		end
+	},
+}
